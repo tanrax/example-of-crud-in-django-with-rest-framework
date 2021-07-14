c877ddbcc9413bdcf87e21716668e612a588def4
Andros Fenollosa <andros@fenollosa.email>
2021-07-14 17:17:52 +0200

    Fix name project and Add Heroku deploy

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "proyect.urls"
+ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "proyect.wsgi.application"
+WSGI_APPLICATION = "project.wsgi.application"
 
 
 # Database
